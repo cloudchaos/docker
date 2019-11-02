5bef0ab14ca97eab039ee73485e1e3ca4fd0a2c1
zorail <rohan.seth497@gmail.com>
2019-11-02 22:57:41 +0530

    minor changes
    Signed-off-by: rohan.seth <rohan.seth@highradius.com>

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,24 +1,28 @@
 #! /bin/sh
 
 export CONTAINER_STATE=$3
+CONTAINER_NAMES=${*: 4:${#@}}
 
-if [ "$CONTAINER_STATE" = 'kill' ]
-then
-    CONTAINER_NAMES=${*: 4:${#@}}
-elif [ "$CONTAINER_STATE" = 'pause' ]
+if [ "$CONTAINER_STATE" = 'pause' ]
 then
     PAUSE_DURATION=$4
     CONTAINER_NAMES=${*: 5:${#@}}
 elif [ "$CONTAINER_STATE" = 'stop' ]
 then
     STOP_DURATION=$4
+    CONTAINER_NAMES=${*: 5:${#@}}
+elif [ "$CONTAINER_STATE" = 'delay' ]
+then
+    NETWORK_SIMULATION_DURATION=$4
+    DELAY_DURATION_IN_MILLIS=$5
+    CONTAINER_NAMES=${*: 6:${#@}}
 fi
 
 export PAUSE_DURATION
 export CONTAINER_NAMES
 export STOP_DURATION
 
-export NETWORK_SIMULATION_DURATION=$4
-export DELAY_DURATION_IN_MILLIS=$5
+export NETWORK_SIMULATION_DURATION
+export DELAY_DURATION_IN_MILLIS
 
 ansible-playbook -i host "$1"/"$2"/chaos_runner.yaml -vv
